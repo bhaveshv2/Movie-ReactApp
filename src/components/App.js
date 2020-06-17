a527a80539213fedcd292382ac75120d8c6b7b9a
bhaveshv2 <bhaveshv2@users.noreply.github.com>
2020-06-17 17:41:55 +0530

    Changing the state to add movies to favourites

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
 import {data} from '../data';
+import {addMovies} from '../actions/index';
 
 class App extends React.Component {
   componentDidMount(){
@@ -14,16 +15,14 @@ class App extends React.Component {
       this.forceUpdate();
     });
 
-    store.dispatch({
-      type:'ADD_MOVIES',
-      movies:data
-    });
+    store.dispatch(addMovies(data));
 
     console.log('STATE',store.getState());
   }
   render(){
-    const movies = this.props.store.getState();
-    console.log('rendered');
+    const {list} = this.props.store.getState(); //{list:[],favourites:[]}
+    
+    console.log('rendered' ,this.props.store.getState());
     return (
       <div className="App">
         <Navbar />
@@ -33,7 +32,7 @@ class App extends React.Component {
             <div className="tab">Favourites</div>
           </div>
           <div className="list">
-            {movies.map((movie,index)=>(
+            {list.map((movie,index)=>(
               <MovieCard movie={movie} key={`movies=${index}`} />
             ))}
           </div>
