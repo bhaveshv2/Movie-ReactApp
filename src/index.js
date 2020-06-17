e0dbc8c7cd46739f5a216cd760aae0405ebca093
bhaveshv2 <bhaveshv2@users.noreply.github.com>
2020-06-17 16:54:12 +0530

    setup the reducer and store getstate, dispatch and subscribe methods

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
+
 import './index.css';
 import App from './components/App';
+import movies from './reducers';
+
+const store = createStore(movies);
+console.log('Store has been uploaded successfully!',store);
+// console.log('BEFORE STATE:',store.getState());
+
+// store.dispatch({
+//     type:'ADD_MOVIES',
+//     movies:[{name:'superman'}]
+// });
+
+// console.log('after STATE:',store.getState());
 
 
-ReactDOM.render(<App />,document.getElementById('root'));
+ReactDOM.render(<App store={store}/>,document.getElementById('root'));
 
