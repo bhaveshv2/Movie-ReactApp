1d9b3bb83d66e2291b3d885819ccdab87c984e59
bhaveshv2 <bhaveshv2@users.noreply.github.com>
2020-06-18 18:00:40 +0530

    Currying, middleware and add logger function to the app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore,applyMiddleware } from 'redux';
 
 
 import './index.css';
 import App from './components/App';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer);
+
+//fumction logger(obj,next,action) ====>   logger(obj)(next)(action)
+
+// const logger = function({dispatch,getState}){
+//     return function(next){
+//         return function(action){
+//             //midlewate here
+//             console.log('Action_Type : ',action.type);
+//             next(action);
+//         }
+//     }
+// }
+
+const logger = ({dispatch,getState}) => (next) => (action)=>{
+    //middleware code
+    console.log('Action_Type : ',action.type);
+    next(action);
+}
+
+const store = createStore(rootReducer,applyMiddleware(logger));
 console.log('Store has been uploaded successfully!',store);
 // console.log('BEFORE STATE:',store.getState());
 
